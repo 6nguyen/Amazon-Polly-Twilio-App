f88f5a4e57066eb2bfbd9d87f8d581d75786f87e
George Nguyen <g.nguyen92@yahoo.com>
2017-09-14 22:31:15 -0700

    Send the audio file

--- server.js
+++ server.js
@@ -23,8 +23,31 @@ var polly = new AWS.Polly();
 var express = require('express')
 var app = express()
 
-// Generate an Audiofile and serve stright back to the user for use as a file stright away
+// Generate an Audiofile and serve stright back to the user for use as
+// a file straight away
 app.get('/play/:voiceId/:textToConvert', function (req, res) {
 var pollyCallback = function (err, data) {
-if (err) console.log(err, err.stack); // an error occurred
-else console.log(data); // successful response
+  if (err) console.log(err, err.stack); // an error occurred
+  else console.log(data); // successful response
+
+
+// Generate a unique name for this audio file, the file name is: PollyVoiceTimeStamp.mp3
+  var filename = req.params.voiceId + (new Date).getTime() + ".mp3";
+  fs.writeFile('./audioFiles/'+filename, data.AudioStream, function (err) {
+    if (err) {
+      console.log('An error occurred while writing the file.');
+      console.log(err);
+    }
+    console.log('Finished writing the file to the filesystem ' + '/audioFiles/'+filename)
+
+
+    // Send the audio file
+    res.setHeader('content-type', 'audio/mpeg');
+    res.download('audioFiles/'+filename);
+  });
+};
+var pollyParameters = {
+OutputFormat: 'mp3',
+Text: unescape(req.params.textToConvert),
+VoiceId: req.params.voiceId
+};
