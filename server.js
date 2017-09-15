29809e4615c436a47629ce58b0d331f3e7703f2e
George Nguyen <g.nguyen92@yahoo.com>
2017-09-15 11:11:06 -0700

    Change app.get HTTP request url to a test using Carla voice

--- server.js
+++ server.js
@@ -25,12 +25,15 @@ var app = express()
 
 // Generate an Audiofile and serve stright back to the user for use as
 // a file straight away
-app.get('/play/:voiceId/:textToConvert', function (req, res) {
+app.get('/play/Carla/Hello%20reader.%20Thank%20you%20for%20taking%20the%20time%20to%20read%20this%20blog%20post%20and%20build%20the%20tutorial.%20I%20Hope%20it%20has%20been%20helpful%20for%20you.
+', function (req, res) {
   var pollyCallback = function (err, data) {
     if (err) console.log(err, err.stack); // an error occurred
     else console.log(data); // successful response
 
-    // Generate a unique name for this audio file, the file name is: PollyVoiceTimeStamp.mp3
+    // Generate a unique name for this audio file: the Polly Voice with the time
+    // stamp when this file was written
+    // the file name is: pollyVoice_timeStamp.mp3
     var filename = req.params.voiceId + (new Date).getTime() + ".mp3";
     fs.writeFile('./audioFiles/'+filename, data.AudioStream, function (err) {
       if (err) {
@@ -41,6 +44,8 @@ app.get('/play/:voiceId/:textToConvert', function (req, res) {
 
 
       // Send the audio file
+      // When the file is downloaded, this is responsible for giving the audio file
+      // an mp3 header
       res.setHeader('content-type', 'audio/mpeg');
       res.download('audioFiles/'+filename);
     });
@@ -51,6 +56,10 @@ app.get('/play/:voiceId/:textToConvert', function (req, res) {
     VoiceId: req.params.voiceId
   };
 
-  // Make a request to AWS Polly with the text and voice needed, when the request is completed push callback to pollyCallback
+  // Make a request to AWS Polly with the text and voice needed, when the request
+  // is completed push callback to pollyCallback
+  // pollyCallback does 2 things:
+  //    Save the file created on fs.writeFile(...) to disk
+  //    Pass the file back to the users request
   polly.synthesizeSpeech(pollyParameters, pollyCallback);
 })
