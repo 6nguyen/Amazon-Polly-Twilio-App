abfcff7d16b6b2e1cd6fad3c64bd9109e1d29e1b
George Nguyen <g.nguyen92@yahoo.com>
2017-09-14 22:24:14 -0700

    Generate Audiofile and serve to user to use as file

--- server.js
+++ server.js
@@ -22,3 +22,9 @@ var polly = new AWS.Polly();
 
 var express = require('express')
 var app = express()
+
+// Generate an Audiofile and serve stright back to the user for use as a file stright away
+app.get('/play/:voiceId/:textToConvert', function (req, res) {
+var pollyCallback = function (err, data) {
+if (err) console.log(err, err.stack); // an error occurred
+else console.log(data); // successful response
