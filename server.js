e372c5045ca77e54ed66b56e59c1bbf5519161c1
George Nguyen <g.nguyen92@yahoo.com>
2017-09-14 22:33:24 -0700

    Make text/voice request to Polly, push callback to pollyCallback

--- server.js
+++ server.js
@@ -26,28 +26,31 @@ var app = express()
 // Generate an Audiofile and serve stright back to the user for use as
 // a file straight away
 app.get('/play/:voiceId/:textToConvert', function (req, res) {
-var pollyCallback = function (err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else console.log(data); // successful response
+  var pollyCallback = function (err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else console.log(data); // successful response
+
+    // Generate a unique name for this audio file, the file name is: PollyVoiceTimeStamp.mp3
+    var filename = req.params.voiceId + (new Date).getTime() + ".mp3";
+    fs.writeFile('./audioFiles/'+filename, data.AudioStream, function (err) {
+      if (err) {
+        console.log('An error occurred while writing the file.');
+        console.log(err);
+      }
+      console.log('Finished writing the file to the filesystem ' + '/audioFiles/'+filename)
 
 
-// Generate a unique name for this audio file, the file name is: PollyVoiceTimeStamp.mp3
-  var filename = req.params.voiceId + (new Date).getTime() + ".mp3";
-  fs.writeFile('./audioFiles/'+filename, data.AudioStream, function (err) {
-    if (err) {
-      console.log('An error occurred while writing the file.');
-      console.log(err);
-    }
-    console.log('Finished writing the file to the filesystem ' + '/audioFiles/'+filename)
+      // Send the audio file
+      res.setHeader('content-type', 'audio/mpeg');
+      res.download('audioFiles/'+filename);
+    });
+  };
+  var pollyParameters = {
+    OutputFormat: 'mp3',
+    Text: unescape(req.params.textToConvert),
+    VoiceId: req.params.voiceId
+  };
 
-
-    // Send the audio file
-    res.setHeader('content-type', 'audio/mpeg');
-    res.download('audioFiles/'+filename);
-  });
-};
-var pollyParameters = {
-OutputFormat: 'mp3',
-Text: unescape(req.params.textToConvert),
-VoiceId: req.params.voiceId
-};
+  // Make a request to AWS Polly with the text and voice needed, when the request is completed push callback to pollyCallback
+  polly.synthesizeSpeech(pollyParameters, pollyCallback);
+})
